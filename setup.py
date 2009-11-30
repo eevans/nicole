49a7ea15b1dacbefe926f3cbbc006225b76cee0b
Eric Evans <eevans@racklabs.com>
2009-11-29 23:10:51 -0600

    prune down repo for distribution

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'nicole',
+    version = '0.1',
+    description = 'Simple blogging platform for Django',
+    author = 'Eric Evans',
+    author_email = 'eevans@sym-link.com',
+    url = 'http://blog.sym-link.com',
+    packages = ['nicole'],
+)
+
+# vi:ai sw=4 ts=4 tw=0 et
